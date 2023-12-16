4dde3ab1b819eddbe2b5827ec0cc39f9a8103880
Solomon <sholasolomon@yahoo.com>
2023-12-16 20:19:11 +0000

    Role and Perissions with Spatie

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -17,21 +17,20 @@
                 <a href="{{ url('amenity/all') }}"><i data-feather="file"></i>
                     <span>Amenity</span></a>
             </li>
+            <li>
+                <a href=""><i data-feather="list"></i>
+                    <span>Role & Permission </span></a>
+                <ul class="sub-menu">
+                    <li class="@if(Request::segment(2) =='permissions') active @endif"><a href="{{url('permission/permissions')}}">Permissions</a>
+                    </li>
+                    <li class="@if(Request::segment(2) =='roles') active @endif"><a href="{{url('role/roles')}}">Roles</a></li>
+                </ul>
+            </li>
             <!-- <li class="@if(Request::segment(2) =='employees') active @endif">
                 <a href="{{ url('admin/employees') }}"><i data-feather="users"></i>
                     <span>Employees</span></a>
             </li>
-            <li>
-                <a href=""><i data-feather="database"></i>
-                    <span>Jobs</span><span class="badge badge-warning ft-right">10+</span></a>
-                <ul class="sub-menu">
-                    <li class="@if(Request::segment(2) =='jobs') active @endif"><a href="{{url('admin/jobs')}}">Jobs</a>
-                    </li>
-                    <li class="@if(Request::segment(2) =='job_history') active @endif"><a href="{{url('admin/job_history')}}">Job History </a></li>
-                    <li class="@if(Request::segment(2) =='grade') active @endif"><a href="{{url('admin/grade')}}">Job
-                            Grade </a></li>
-                </ul>
-            </li>
+            
             <li class="@if(Request::segment(2) =='department') active @endif">
                 <a href="{{ url('admin/department') }}"><i data-feather="home"></i>
                     <span>Department</span></a>
